93b758bcbc5fd37049a2f2f64434e2545e036abe
moslisnas <molicapacidad@gmail.com>
2018-08-31 18:08:05 +0200

    Adding image views.

--- Vulkan/Windows x86/RenderEngineV0.0/RenderEngineV0.0/main.cpp
+++ Vulkan/Windows x86/RenderEngineV0.0/RenderEngineV0.0/main.cpp
@@ -68,12 +68,15 @@ private:
 	VkSurfaceKHR surface;
 	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 	VkDevice device;
+	//Queues
 	VkQueue graphicsQueue;
 	VkQueue presentQueue;
+	//Swap chain
 	VkSwapchainKHR swapChain;
 	std::vector<VkImage> swapChainImages;
 	VkFormat swapChainImageFormat;
 	VkExtent2D swapChainExtent;
+	std::vector<VkImageView> swapChainImageViews;
 
 	void initWindow(){
 		glfwInit();
@@ -90,6 +93,7 @@ private:
 		pickPhysicalDevice();
 		createLogicalDevice();
 		createSwapChain();
+		createImageViews();
 	}
 
 	void createInstance(){
@@ -436,6 +440,31 @@ private:
 			return actualExtent;
 		}
 	}
+	//Swap chain image views
+	void createImageViews(){
+		swapChainImageViews.resize(swapChainImages.size());
+
+		for(size_t i=0; i<swapChainImages.size(); i++){
+			//Creation info
+			VkImageViewCreateInfo createInfo ={};
+			createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			createInfo.image = swapChainImages[i];
+			createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+			createInfo.format = swapChainImageFormat;
+			createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			createInfo.subresourceRange.baseMipLevel = 0;
+			createInfo.subresourceRange.levelCount = 1;
+			createInfo.subresourceRange.baseArrayLayer = 0;
+			createInfo.subresourceRange.layerCount = 1;
+			//Creation
+			if(vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS)
+				throw std::runtime_error("failed to create image views!");
+		}
+	}
 
 	void mainLoop(){
 		while(!glfwWindowShouldClose(window)) {
@@ -444,6 +473,9 @@ private:
 	}
 
 	void cleanup(){
+		//Image views
+		for(auto imageView:swapChainImageViews)
+			vkDestroyImageView(device, imageView, nullptr);
 		//Swap chain
 		vkDestroySwapchainKHR(device, swapChain, nullptr);
 		//Logical device
