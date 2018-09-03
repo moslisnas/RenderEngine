43cad211530cdd9be37b2f1dc76d7a75590b04b0
moslisnas <molicapacidad@gmail.com>
2018-09-03 09:02:20 +0200

    Adding framebuffers.

--- Vulkan/Windows x86/RenderEngineV0.0/RenderEngineV0.0/main.cpp
+++ Vulkan/Windows x86/RenderEngineV0.0/RenderEngineV0.0/main.cpp
@@ -83,6 +83,8 @@ private:
 	//Pipeline
 	VkPipelineLayout pipelineLayout;
 	VkPipeline graphicsPipeline;
+	//Framebuffers
+	std::vector<VkFramebuffer> swapChainFramebuffers;
 
 	void initWindow(){
 		glfwInit();
@@ -102,6 +104,7 @@ private:
 		createImageViews();
 		createRenderPass();
 		createGraphicsPipeline();
+		createFramebuffers();
 	}
 	//Instance
 	void createInstance(){
@@ -682,6 +685,24 @@ private:
 		return shaderModule;
 	}
 
+	//Framebuffers
+	void createFramebuffers(){
+		swapChainFramebuffers.resize(swapChainImageViews.size());
+
+		for(size_t i=0; i<swapChainImageViews.size(); i++){
+			VkImageView attachments[] = {swapChainImageViews[i]};
+			VkFramebufferCreateInfo framebufferInfo ={};
+			framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+			framebufferInfo.renderPass = renderPass;
+			framebufferInfo.attachmentCount = 1;
+			framebufferInfo.pAttachments = attachments;
+			framebufferInfo.width = swapChainExtent.width;
+			framebufferInfo.height = swapChainExtent.height;
+			framebufferInfo.layers = 1;
+			if(vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapChainFramebuffers[i]) != VK_SUCCESS)
+				throw std::runtime_error("failed to create framebuffer!");
+		}
+	}
 
 	void mainLoop(){
 		while(!glfwWindowShouldClose(window)) {
@@ -690,6 +711,9 @@ private:
 	}
 
 	void cleanup(){
+		//Framebuffers
+		for(auto framebuffer : swapChainFramebuffers)
+			vkDestroyFramebuffer(device, framebuffer, nullptr);
 		//Render pass
 		vkDestroyRenderPass(device, renderPass, nullptr);
 		//Pipeline layout
