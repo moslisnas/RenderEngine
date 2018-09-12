1f7687ca4d0e55b0060ac6c3f997c1c539284ae4
moslisnas <molicapacidad@gmail.com>
2018-09-12 03:52:15 +0200

    Adding imported models & performing index and vertex storage.

--- Vulkan/Windows x86/RenderEngineV0.0/RenderEngineV0.0/main.cpp
+++ Vulkan/Windows x86/RenderEngineV0.0/RenderEngineV0.0/main.cpp
@@ -17,13 +17,22 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <chrono>
-
+//Textures
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+//Models
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+#include <unordered_map>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
 const int MAX_FRAMES_IN_FLIGHT = 2;
+//Model
+const std::string MODEL_PATH = "models/castillo.obj";
+const std::string TEXTURE_PATH = "textures/test_diff.jpg";
 
 const std::vector<const char*> validationLayers ={"VK_LAYER_LUNARG_standard_validation"};
 const std::vector<const char*> deviceExtensions ={ VK_KHR_SWAPCHAIN_EXTENSION_NAME };
@@ -97,7 +106,17 @@ struct Vertex {
 
 		return attributeDescriptions;
 	}
+	bool operator==(const Vertex& other) const{
+		return pos == other.pos && color == other.color && texCoord	== other.texCoord;
+	}
 };
+namespace std{
+	template<> struct hash<Vertex>{
+		size_t operator()(Vertex const& vertex) const{
+			return ((hash<glm::vec3>()(vertex.pos) ^ (hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^ (hash<glm::vec2>()(vertex.texCoord) << 1);
+		}
+	};
+}
 struct UniformBufferObject {
 	glm::mat4 model;
 	glm::mat4 view;
@@ -119,7 +138,10 @@ public:
 		app->framebufferResized = true;
 	}
 	//Data members
-	const std::vector<Vertex> vertices ={
+	std::vector<Vertex> vertices;
+	std::vector<uint32_t> indices;
+	//RECTANGLE
+	/*const std::vector<Vertex> vertices ={
 		//First square
 		{{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
 		{{0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
@@ -134,7 +156,7 @@ public:
 	const std::vector<uint16_t> indices ={
 		0, 1, 2, 2, 3, 0,
 		4, 5, 6, 6, 7, 4
-	};
+	};*/
 private:
 	#pragma region Data members
 	GLFWwindow * window;
@@ -222,6 +244,7 @@ private:
 		createTextureImage();
 		createTextureImageView();
 		createTextureSampler();
+		loadModel();
 		createVertexBuffer();
 		createIndexBuffer();
 		createUniformBuffer();
@@ -1165,7 +1188,8 @@ private:
 			VkBuffer vertexBuffers[] = {vertexBuffer};
 			VkDeviceSize offsets[] = {0};
 			vkCmdBindVertexBuffers(commandBuffers[i], 0, 1, vertexBuffers, offsets);
-			vkCmdBindIndexBuffer(commandBuffers[i], indexBuffer, 0, VK_INDEX_TYPE_UINT16);
+			vkCmdBindIndexBuffer(commandBuffers[i], indexBuffer, 0, VK_INDEX_TYPE_UINT32);
+			//vkCmdBindIndexBuffer(commandBuffers[i], indexBuffer, 0, VK_INDEX_TYPE_UINT16); RECTANGLE
 			//Uniform buffers
 			vkCmdBindDescriptorSets(commandBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 0, 1, &descriptorSets[i], 0, nullptr);
 			//Draw
@@ -1233,7 +1257,8 @@ private:
 	void createTextureImage(){
 		//Loading file
 		int texWidth, texHeight, texChannels;
-		stbi_uc* pixels = stbi_load("textures/texture.jpg", &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+		stbi_uc * pixels = stbi_load(TEXTURE_PATH.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+		//stbi_uc* pixels = stbi_load("textures/texture.jpg", &texWidth, &texHeight, &texChannels, STBI_rgb_alpha); //RECTANGLE
 		VkDeviceSize imageSize = texWidth * texHeight * 4;
 		if(!pixels)
 			throw std::runtime_error("failed to load texture image!");
@@ -1357,6 +1382,42 @@ private:
 		vkCmdCopyBufferToImage(commandBuffer, buffer, image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &region);
 		//End command buffer
 		endSingleTimeCommands(commandBuffer);
+	}
+	#pragma endregion
+
+	#pragma region Models
+	void loadModel(){
+		tinyobj::attrib_t attrib;
+		std::vector<tinyobj::shape_t> shapes;
+		std::vector<tinyobj::material_t> materials;
+		std::string err;
+
+		//Loading
+		if(!tinyobj::LoadObj(&attrib, &shapes, &materials, &err, MODEL_PATH.c_str())) 
+			throw std::runtime_error(err);
+		//Filling data
+		std::unordered_map<Vertex, uint32_t> uniqueVertices ={};
+		for(const auto & shape : shapes){
+			for(const auto & index : shape.mesh.indices) {
+				Vertex vertex = {};
+
+				vertex.pos = {
+					attrib.vertices[3 * index.vertex_index + 0]/5.0f,
+					attrib.vertices[3 * index.vertex_index + 1]/5.0f,
+					attrib.vertices[3 * index.vertex_index + 2]/5.0f
+				};
+				vertex.texCoord ={
+					attrib.texcoords[2 * index.texcoord_index + 0],
+					1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
+				};
+				vertex.color ={1.0f, 1.0f, 1.0f};
+				if(uniqueVertices.count(vertex) == 0){
+					uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+					vertices.push_back(vertex);
+				}
+				indices.push_back(uniqueVertices[vertex]);
+			}
+		}
 	}
 	#pragma endregion
 
